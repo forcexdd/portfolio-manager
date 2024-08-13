32d2069fc4c4db8f629f5d6498e1420c3dc41f00
Nikita Sharudin <nikita.sharudin@mail.ru>
2024-08-13 19:00:43 +0300

    adjusted main to new interaction with db

--- src/cmd/app/main.go
+++ src/cmd/app/main.go
@@ -8,12 +8,13 @@ import (
 func main() {
 	const connString = "postgresql://postgres:postgres@localhost:5432/StockPortfolioManager?sslmode=disable"
 
-	err := database.DeleteStorage(connString)
+	_, err := database.CreateNewStorage(connString)
 	if err != nil {
 		log.Fatal(err)
 	}
-	_, err = database.CreateNewStorage(connString)
-	if err != nil {
-		log.Fatal(err)
-	}
+
+	//err = storage.DeleteStorage()
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
 }
