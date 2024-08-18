0cf7641970cddbbde269ea42a5ae78c6fe501b69
Nikita Sharudin <nikita.sharudin@mail.ru>
2024-08-18 21:32:54 +0300

    some testing in main

--- src/cmd/app/main.go
+++ src/cmd/app/main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
+	"github.com/forcexdd/StockPortfolioManager/src/web/backend/database/repositories"
 	"github.com/forcexdd/StockPortfolioManager/src/web/backend/database/storage"
+	"github.com/forcexdd/StockPortfolioManager/src/web/backend/models"
 	"log"
 )
 
 func main() {
 	const connString = "postgresql://postgres:postgres@localhost:5432/StockPortfolioManager?sslmode=disable"
 
-	_, err := storage.NewStorage(connString)
+	db, err := storage.NewStorage(connString)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	//err = storage.DeleteStorage()
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
+	stockRepository := repositories.NewStockRepository(db.GetDb())
+	portfolioRepository := repositories.NewPortfolioRepository(db.GetDb())
+
+	stock := &models.Stock{Name: "AFLT", Price: 123.4567}
+	stockRepository.Delete(stock)
+
+	//newStock, err := stockRepository.GetByName("AFLT")
+	newMap := map[*models.Stock]int{
+		stock: 3,
+	}
+
+	portfolioRepository.Delete(&models.Portfolio{Name: "Aboba portfel", StocksQuantityMap: newMap})
+	db.CloseConnection()
 }
