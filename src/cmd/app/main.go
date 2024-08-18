4948f2f7e773b7d57a306b36c8d527fd3bbde72e
Elisey Kalashnikov <elisey0380231@yandex.ru>
2024-08-19 00:36:42 +0300

    Revert "Merged develope to a feature/site"
    This reverts commit 78363b3c716efd5a875fec581dc1ed3dc8c1af72, reversing
    changes made to ecad8b3722db9c09eeb1854aa91bbd7ec089e630.

--- src/cmd/app/main.go
+++ src/cmd/app/main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"net/http"
-	"github.com/forcexdd/StockPortfolioManager/src/web/backend/database"
-	"github.com/forcexdd/StockPortfolioManager/src/web/backend/handlers"
-	"log"
 )
+import "github.com/forcexdd/StockPortfolioManager/src/web/backend/handlers"
 
 func main() {
 	http.HandleFunc("/static/", handlers.HandleStaticFiles)
@@ -13,23 +11,9 @@ func main() {
 	http.HandleFunc("/following_index", handlers.HandleHome)
 	http.HandleFunc("/manager", handlers.HandleManager)
 	http.HandleFunc("/add_portfolio", handlers.HandleAddPortfolio)
-	
-	const connString = "postgresql://postgres:postgres@localhost:5432/StockPortfolioManager?sslmode=disable"
 
-	_, err := database.CreateNewStorage(connString)
+	err := http.ListenAndServe("localhost:8080", nil)
 	if err != nil {
-		log.Fatal(err)
-		return
+		panic(err)
 	}
-	
-	err = http.ListenAndServe("localhost:8080", nil)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	//err = storage.DeleteStorage()
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
 }
