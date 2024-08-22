6fbc781fda6f8f294f8bc0832b5a9b81ea87f6f6
Nikita Sharudin <nikita.sharudin@mail.ru>
2024-08-23 00:25:33 +0300

    changed main for testing api client

--- src/cmd/app/main.go
+++ src/cmd/app/main.go
@@ -1,37 +1,14 @@
 package main
 
 import (
-	"github.com/forcexdd/StockPortfolioManager/src/web/backend/database/storage"
-	"log"
+	"fmt"
+	"github.com/forcexdd/StockPortfolioManager/src/web/backend/services/moex_api_client"
 )
 
 func main() {
-	const connString = "postgresql://postgres:postgres@localhost:5432/StockPortfolioManager?sslmode=disable"
-
-	db, err := storage.NewStorage(connString)
-	if err != nil {
-		log.Fatal(err)
+	apiClient := moex_api_client.NewMoexApiClient()
+	a, _ := apiClient.GetAllStocks("2024-07-25")
+	for _, b := range a {
+		fmt.Println(b)
 	}
-
-	//stockRepository := repositories.NewStockRepository(db.GetDb())
-	//portfolioRepository := repositories.NewPortfolioRepository(db.GetDb())
-	//
-	//stockAFLT := &models.Stock{Name: "AFLT", Price: 123.4567}
-	//stockGAZP := &models.Stock{Name: "GAZP", Price: 456}
-	//stockALRS := &models.Stock{Name: "ALRS", Price: 45.12}
-	//newMap := map[*models.Stock]int{
-	//	stockAFLT: 1,
-	//	stockGAZP: 34,
-	//	stockALRS: 9,
-	//}
-	//
-	//newPortfolio := &models.Portfolio{Name: "Aboba", StocksQuantityMap: newMap}
-	//
-	//stockRepository.Create(stockAFLT)
-	//stockRepository.Create(stockGAZP)
-	//stockRepository.Create(stockALRS)
-	//
-	//portfolioRepository.Create(newPortfolio)
-
-	db.CloseConnection()
 }
